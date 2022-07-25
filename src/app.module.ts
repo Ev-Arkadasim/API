f46593bb50de5d8fc67c466b29ae165296d42d00
Mustafa Kendigüzel <mustafa.kendiguzel@bil.omu.edu.tr>
2022-07-25 09:23:43 +0300

    change connnection name

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import {MongooseModule} from '@nestjs/mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb+srv://karahan1:karahan@cluster0.xqacgqt.mongodb.net/vinc?retryWrites=true&w=majority'),
-    UsersModule
-    ]
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), UsersModule],
 })
 export class AppModule {}
